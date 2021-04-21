d7ee690c77adc7b88b65a5df683e242d475aec4f
AkatQuas <295140755@qq.com>
2021-04-21 19:44:33 +0800

    feat: using nx.dev to build monorepo

---
+++ nxdev/nrwl-todos/libs/feature-sets/src/lib/set-list/set-list.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SetListComponent } from './set-list.component';
+
+describe('SetListComponent', () => {
+  let component: SetListComponent;
+  let fixture: ComponentFixture<SetListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SetListComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SetListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
