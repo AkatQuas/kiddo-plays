3e9acd4c0b0b53d03014001e6654bf3739f42746
AkatQuas <295140755@qq.com>
2022-04-09 09:43:43 +0800

    feat(nxdev): add demo using "core" preset

---
+++ nxdev/README.md
@@ -0,0 +1,3 @@
+# Nx.dev projects
+
+Some small demo projects built upon [Nx.dev](https://nx.dev/).
---
+++ nxdev/kfc/packages/messages/index.js
@@ -0,0 +1,3 @@
+const message = 'Welcome to the Restaurant 42 at the End of the Universe';
+
+module.exports = { message };
