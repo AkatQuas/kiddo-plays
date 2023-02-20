79ce8f9ce62d823453bf9db1aed82c2115a7e415
AkatQuas <295140755@qq.com>
2023-02-20 20:35:38 +0800

    feat: wechat mini program

---
+++ wxma/vanilla-ma/utils/util2.js
@@ -0,0 +1,13 @@
+const a = [{
+  event: 'ide_compile',
+  params: '{"result":"success","scene":"ide","appid":"tteff0994d70c8128d","watch":0,"compileDimension":"sub","ideChromeVersion":"91","useCache":"true","generateModules":11,"compilerVersion":"1.9.2"}'
+}, {
+  event: 'ide_compile',
+  params: '{"result":"success","scene":"ide","appid":"tteff0994d70c8128d","watch":0,"compileDimension":"sub","ideChromeVersion":"91","useCache":"true","generateChunks":0,"compilerVersion":"1.9.2"}'
+}, {
+  event: 'ide_compile',
+  params: '{"result":"success","scene":"ide","appid":"tteff0994d70c8128d","watch":0,"compileDimension":"sub","ideChromeVersion":"91","useCache":"true","generateAssets":10,"compilerVersion":"1.9.2"}'
+}, {
+  event: 'ide_compile',
+  params: '{"result":"success","scene":"ide","appid":"tteff0994d70c8128d","watch":0,"compileDimension":"sub","ideChromeVersion":"91","useCache":"true","buildEnd":0,"compilerVersion":"1.9.2"}'
+}, ]
